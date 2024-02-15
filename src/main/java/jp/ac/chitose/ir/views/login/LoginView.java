c65b107d4a02bd15e26c3b8013aac521f7de6438
katatsumuri <maimaidance0620@gmail.com>
2024-02-15 19:29:46 +0900

    ログイン画面の追加

---
+++ src/main/java/jp/ac/chitose/ir/views/login/LoginView.java
@@ -0,0 +1,12 @@
+package jp.ac.chitose.ir.views.login;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.RouteAlias;
+import jp.ac.chitose.ir.views.MainLayout;
+
+@PageTitle("login")
+@RouteAlias(value = "login", layout = MainLayout.class)
+public class LoginView extends VerticalLayout {
+    //test
+}
