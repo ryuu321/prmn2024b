ef4572373f021f615984a2c14930cc99e5092b86
katatsumuri <maimaidance0620@gmail.com>
2024-02-16 13:25:15 +0900

    vaadinのチュートリアルを参考にlogin画面を作成

--- src/main/java/jp/ac/chitose/ir/views/login/LoginView.java
+++ src/main/java/jp/ac/chitose/ir/views/login/LoginView.java
@@ -1,12 +1,41 @@
 package jp.ac.chitose.ir.views.login;
 
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
-import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
 import jp.ac.chitose.ir.views.MainLayout;
 
 @PageTitle("login")
-@RouteAlias(value = "login", layout = MainLayout.class)
-public class LoginView extends VerticalLayout {
-    //test
+@Route(value = "login", layout = MainLayout.class)
+@AnonymousAllowed
+public class LoginView extends VerticalLayout implements BeforeEnterObserver {
+    private LoginForm login = new LoginForm();
+
+    public LoginView() {
+            addClassName("login-view");
+            setSizeFull();
+
+            setJustifyContentMode(JustifyContentMode.CENTER);
+            setAlignItems(Alignment.CENTER);
+
+            login.setAction("login");
+
+            add(new H1("Test Application"), login);
+        }
+
+        @Override
+        public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
+            if(beforeEnterEvent.getLocation()
+                    .getQueryParameters()
+                    .getParameters()
+                    .containsKey("error")) {
+                login.setError(true);
+            }
+
+    }
 }
