bb12d20fed020e3739497a02c68c44f81b9b2649
ymdyt12 <b2222480@photon.chitose.ac.jp>
2024-03-19 11:53:05 +0900

    IRアンケートのレイアウトの変更

---
+++ src/main/java/jp/ac/chitose/ir/views/commission/ir/Question.java
@@ -0,0 +1,36 @@
+package jp.ac.chitose.ir.views.commission.ir;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
+
+public class Question {
+    private String questionName;
+    private String question;
+
+    public Question(String questionName,String question){
+        this.questionName = questionName;
+        this.question = question;
+    }
+
+    public VerticalLayout view(){
+        VerticalLayout main = new VerticalLayout();
+
+        main.add(new H2(question));
+        main.add(new Paragraph(("説明文")));//今後どうするか考える
+
+        RadioButtonGroup<String> radioGroup = new RadioButtonGroup<>();
+        radioGroup.setLabel("学科");
+        radioGroup.setItems("全体", "応用科学生物学科","電子光工学科","情報システム工学科");
+        main.add(radioGroup);
+
+        main.setVisible(false);
+
+        return main;
+    }
+
+    public String getQuestionName(){
+        return questionName;
+    }
+}
