89c1382b7a761aa27689cbcf43260978fdc2a298
natsumikan49 <reality.chrosigu.glycin.96@gmail.com>
2024-03-28 16:37:12 +0900

    レコードの変更

--- src/main/java/jp/ac/chitose/ir/service/class_select/ClassQPOJFICHKVJB.java
+++ src/main/java/jp/ac/chitose/ir/service/class_select/ClassQPOJFICHKVJB.java
@@ -1,26 +1,30 @@
 package jp.ac.chitose.ir.service.class_select;
 
 public record ClassQPOJFICHKVJB(
-        String 科目名,
-        String 担当者,
-        String q4,
-        String q5,
-        String q6,
-        String q7,
-        String q8,
-        String q9,
-        String q10,
-        String q11,
-        String q12,
-        String q13,
-        String q14,
-        String q15,
-        String q16,
-        String q17,
-        String q18,
-        String q19
-
-
+        String q4_項目,
+        float q4_割合,
+        String q5_項目,
+        float q5_割合,
+        String q6_項目,
+        float q6_割合,
+        String q8_項目,
+        float q8_割合,
+        String q9_項目,
+        float q9_割合,
+        String q10_項目,
+        float q10_割合,
+        String q11_項目,
+        float q11_割合,
+        String q12_項目,
+        float q12_割合,
+        String q13_項目,
+        float q13_割合,
+        String q14_項目,
+        float q14_割合,
+        String q15_項目,
+        float q15_割合,
+        String q16_項目,
+        float q16_割合
 
 ) {
 }
