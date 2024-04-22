fdaef6a8a2c6e8117c5e2de0d08c408ebcb848ab
ymdyt12 <b2222480@photon.chitose.ac.jp>
2024-04-22 16:04:25 +0900

    1-4年のデータを追加

--- src/main/java/jp/ac/chitose/ir/service/commission/CommissionService.java
+++ src/main/java/jp/ac/chitose/ir/service/commission/CommissionService.java
@@ -12,4 +12,28 @@ public interface CommissionService {
     @GetExchange("/gpa2")
     TableData<CommissionGpa2>getCommissionGpa2();
 
+    @GetExchange("/gpa/first")
+    TableData<CommissionGpaFirst>getCommissionGpaFirst();
+
+    @GetExchange("/gpa2/first")
+    TableData<CommissionGpa2First>getCommissionGpa2First();
+
+    @GetExchange("/gpa/second")
+    TableData<CommissionGpaSecond>getCommissionGpaSecond();
+
+    @GetExchange("/gpa2/second")
+    TableData<CommissionGpa2Second>getCommissionGpa2Second();
+
+    @GetExchange("/gpa/third")
+    TableData<CommissionGpaThird>getCommissionGpaThird();
+
+    @GetExchange("/gpa2/third")
+    TableData<CommissionGpa2Third>getCommissionGpa2Third();
+
+    @GetExchange("/gpa/fourth")
+    TableData<CommissionGpaFourth>getCommissionGpaFourth();
+
+    @GetExchange("/gpa2/fourth")
+    TableData<CommissionGpa2Fourth>getCommissionGpa2Fourth();
+
 }
