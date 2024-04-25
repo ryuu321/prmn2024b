cc10f6be051da86b156d02b47bcc8c5bd40af9f9
[qwlrn] <[integral_15@yahoo.co.jp]>
2024-04-25 12:03:12 +0900

    グラフ表示の変更

--- src/main/java/jp/ac/chitose/ir/views/commission/seiseki/SeisekiGraphView.java
+++ src/main/java/jp/ac/chitose/ir/views/commission/seiseki/SeisekiGraphView.java
@@ -23,18 +23,24 @@ public class SeisekiGraphView {
         HorizontalLayout layout=new HorizontalLayout();
         HorizontalLayout layout2=new HorizontalLayout();
         ArrayList<VerticalLayout> chartList=seisekiGraph.makeAll();
+        //ArrayList<VerticalLayout> chartList1=seisekiGraph.makeAll();
+
         for(VerticalLayout chart: chartList){
             layout.add(chart);
             layout2.add(chart);
         }
+
+
         main.add(layout,layout2);
+        //main.add(layout2);
         main.setVisible(false);
+
         seiseki.getR1().addValueChangeListener(e->main.setVisible(e.getValue().equals("全体")));
-        seiseki.getR2().addValueChangeListener(e -> chartList.get(0).setVisible(e.getValue().equals("全体")));
-        seiseki.getR2().addValueChangeListener(e -> chartList.get(1).setVisible(e.getValue().equals("応用化学生物学科")));
-        seiseki.getR2().addValueChangeListener(e -> chartList.get(2).setVisible(e.getValue().equals("電子光工学科")));
-        seiseki.getR2().addValueChangeListener(e -> chartList.get(3).setVisible(e.getValue().equals("情報システム工学科")));
-        seiseki.getR2().addValueChangeListener(e -> chartList.get(4).setVisible(e.getValue().equals("一年生")));
+        seiseki.getR2().addValueChangeListener(e -> chartList.get(0).setVisible(!(e.getValue().equals("全体"))));
+        seiseki.getR2().addValueChangeListener(e -> chartList.get(1).setVisible(!(e.getValue().equals("応用化学生物学科"))));
+        seiseki.getR2().addValueChangeListener(e -> chartList.get(2).setVisible(!(e.getValue().equals("電子光工学科"))));
+        seiseki.getR2().addValueChangeListener(e -> chartList.get(3).setVisible(!(e.getValue().equals("情報システム工学科"))));
+        seiseki.getR2().addValueChangeListener(e -> chartList.get(4).setVisible(!(e.getValue().equals("一年生"))));
         seiseki.getR2().addValueChangeListener(e -> {
             if (e.getValue().equals("全て")) {
                 for (VerticalLayout chart : chartList) {
