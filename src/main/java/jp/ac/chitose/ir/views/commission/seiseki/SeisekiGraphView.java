80bad2063edef0f74c175cb575ff41a0d888a80f
[qwlrn] <[integral_15@yahoo.co.jp]>
2024-05-01 21:51:03 +0900

    成績çグラフ作成のクラス作成

--- src/main/java/jp/ac/chitose/ir/views/commission/seiseki/SeisekiGraphView.java
+++ src/main/java/jp/ac/chitose/ir/views/commission/seiseki/SeisekiGraphView.java
@@ -44,13 +44,13 @@ public class SeisekiGraphView {
         HorizontalLayout layout=new HorizontalLayout();
         HorizontalLayout layout2=new HorizontalLayout();
         ArrayList<VerticalLayout> chartList=seisekiGraph.makeAll();
-        ArrayList<VerticalLayout> chartList1=seisekiGraph.makeBigAll();
+        ArrayList<VerticalLayout> chartListB=seisekiGraph.makeBigAll();
 
         for(VerticalLayout chart: chartList){
             layout.add(chart);
             //layout2.add(chart);
         }
-        for(VerticalLayout chart: chartList1){
+        for(VerticalLayout chart: chartListB){
             //layout.add(chart);
             layout2.add(chart);
             chart.setVisible(false);
@@ -60,28 +60,28 @@ public class SeisekiGraphView {
         all.add(layout2);
         all.add(layout);
 
-//        seiseki.getR1().addValueChangeListener(e->all.setVisible(e.getValue().equals("全体")));
+
         seiseki.getR2().addValueChangeListener(e -> {
             chartList.get(0).setVisible(!(e.getValue().equals("全体")));
-            chartList1.get(0).setVisible(e.getValue().equals("全体"));
+            chartListB.get(0).setVisible(e.getValue().equals("全体"));
                 }
             );
         seiseki.getR2().addValueChangeListener(e -> {
             chartList.get(1).setVisible(!(e.getValue().equals("応用化学生物学科")));
-            chartList1.get(1).setVisible(e.getValue().equals("応用化学生物学科"));
+            chartListB.get(1).setVisible(e.getValue().equals("応用化学生物学科"));
         });
         seiseki.getR2().addValueChangeListener(e -> {
             chartList.get(2).setVisible(!(e.getValue().equals("電子光工学科")));
-            chartList1.get(2).setVisible(e.getValue().equals("電子光工学科"));
+            chartListB.get(2).setVisible(e.getValue().equals("電子光工学科"));
 
         });
         seiseki.getR2().addValueChangeListener(e -> {
             chartList.get(3).setVisible(!(e.getValue().equals("情報システム工学科")));
-            chartList1.get(3).setVisible(e.getValue().equals("情報システム工学科"));
+            chartListB.get(3).setVisible(e.getValue().equals("情報システム工学科"));
         });
         seiseki.getR2().addValueChangeListener(e -> {
             chartList.get(4).setVisible(!(e.getValue().equals("一年生")));
-            chartList1.get(4).setVisible(e.getValue().equals("一年生"));
+            chartListB.get(4).setVisible(e.getValue().equals("一年生"));
 
         });
         seiseki.getR2().addValueChangeListener(e -> {
@@ -94,19 +94,197 @@ public class SeisekiGraphView {
         //ここまでall
 
         //ここからfirst
+        HorizontalLayout layout3=new HorizontalLayout();
+        HorizontalLayout layout4=new HorizontalLayout();
+        ArrayList<VerticalLayout> chartList1=seisekiGraph.makeFirst();
+        ArrayList<VerticalLayout> chartList1B=seisekiGraph.makeBigFirst();
 
+        for(VerticalLayout chart: chartList1){
+            layout4.add(chart);
+            //layout2.add(chart);
+        }
+        for(VerticalLayout chart: chartList1B){
+            //layout.add(chart);
+            layout3.add(chart);
+            chart.setVisible(false);
+        }
+
+
+        first.add(layout3);
+        first.add(layout4);
+
+
+        seiseki.getR3().addValueChangeListener(e -> {
+                    chartList1.get(0).setVisible(!(e.getValue().equals("学年全体")));
+                    chartList1B.get(0).setVisible(e.getValue().equals("学年全体"));
+                }
+        );
+        seiseki.getR3().addValueChangeListener(e -> {
+            chartList1.get(1).setVisible(!(e.getValue().equals("Aクラス")));
+            chartList1B.get(1).setVisible(e.getValue().equals("Aクラス"));
+        });
+        seiseki.getR3().addValueChangeListener(e -> {
+            chartList1.get(2).setVisible(!(e.getValue().equals("Bクラス")));
+            chartList1B.get(2).setVisible(e.getValue().equals("Bクラス"));
+
+        });
+        seiseki.getR3().addValueChangeListener(e -> {
+            chartList1.get(3).setVisible(!(e.getValue().equals("Cクラス")));
+            chartList1B.get(3).setVisible(e.getValue().equals("Cクラス"));
+        });
+        seiseki.getR3().addValueChangeListener(e -> {
+            chartList1.get(4).setVisible(!(e.getValue().equals("Dクラス")));
+            chartList1B.get(4).setVisible(e.getValue().equals("Dクラス"));
+
+        });
+        seiseki.getR3().addValueChangeListener(e -> {
+            if (e.getValue().equals("全て")) {
+                for (VerticalLayout chart : chartList1) {
+                    chart.setVisible(true);
+                }
+            }
+        });
         //ここまでfirst
 
         //ここからsecond
+        HorizontalLayout layout5=new HorizontalLayout();
+        HorizontalLayout layout6=new HorizontalLayout();
+        ArrayList<VerticalLayout> chartList2=seisekiGraph.makeSecond();
+        ArrayList<VerticalLayout> chartList2B=seisekiGraph.makeBigSecond();
 
+        for(VerticalLayout chart: chartList2){
+            layout6.add(chart);
+        }
+        for(VerticalLayout chart: chartList2B){
+            layout5.add(chart);
+            chart.setVisible(false);
+        }
+
+
+        second.add(layout5);
+        second.add(layout6);
+
+
+        seiseki.getR4().addValueChangeListener(e -> {
+                    chartList2.get(0).setVisible(!(e.getValue().equals("学年全体")));
+                    chartList2B.get(0).setVisible(e.getValue().equals("学年全体"));
+                }
+        );
+        seiseki.getR4().addValueChangeListener(e -> {
+            chartList2.get(1).setVisible(!(e.getValue().equals("応用化学生物学科")));
+            chartList2B.get(1).setVisible(e.getValue().equals("応用化学生物学科"));
+        });
+        seiseki.getR4().addValueChangeListener(e -> {
+            chartList2.get(2).setVisible(!(e.getValue().equals("電子光工学科")));
+            chartList2B.get(2).setVisible(e.getValue().equals("電子光工学科"));
+
+        });
+        seiseki.getR4().addValueChangeListener(e -> {
+            chartList2.get(3).setVisible(!(e.getValue().equals("情報システム工学科")));
+            chartList2B.get(3).setVisible(e.getValue().equals("情報システム工学科"));
+        });
+
+        seiseki.getR2().addValueChangeListener(e -> {
+            if (e.getValue().equals("全て")) {
+                for (VerticalLayout chart : chartList2) {
+                    chart.setVisible(true);
+                }
+            }
+        });
         //ここまでsecond
 
         //ここからthird
+        HorizontalLayout layout7=new HorizontalLayout();
+        HorizontalLayout layout8=new HorizontalLayout();
+        ArrayList<VerticalLayout> chartList3=seisekiGraph.makeThird();
+        ArrayList<VerticalLayout> chartList3B=seisekiGraph.makeBigThird();
 
+        for(VerticalLayout chart: chartList3){
+            layout8.add(chart);
+        }
+        for(VerticalLayout chart: chartList3B){
+            layout7.add(chart);
+            chart.setVisible(false);
+        }
+
+
+        third.add(layout7);
+        third.add(layout8);
+
+
+        seiseki.getR5().addValueChangeListener(e -> {
+                    chartList3.get(0).setVisible(!(e.getValue().equals("学年全体")));
+                    chartList3B.get(0).setVisible(e.getValue().equals("学年全体"));
+                }
+        );
+        seiseki.getR5().addValueChangeListener(e -> {
+            chartList3.get(1).setVisible(!(e.getValue().equals("応用化学生物学科")));
+            chartList3B.get(1).setVisible(e.getValue().equals("応用化学生物学科"));
+        });
+        seiseki.getR5().addValueChangeListener(e -> {
+            chartList3.get(2).setVisible(!(e.getValue().equals("電子光工学科")));
+            chartList3B.get(2).setVisible(e.getValue().equals("電子光工学科"));
+
+        });
+        seiseki.getR5().addValueChangeListener(e -> {
+            chartList3.get(3).setVisible(!(e.getValue().equals("情報システム工学科")));
+            chartList3B.get(3).setVisible(e.getValue().equals("情報システム工学科"));
+        });
+
+        seiseki.getR5().addValueChangeListener(e -> {
+            if (e.getValue().equals("全て")) {
+                for (VerticalLayout chart : chartList3) {
+                    chart.setVisible(true);
+                }
+            }
+        });
         //ここまでthird
 
         //ここからfourth
+        HorizontalLayout layout9=new HorizontalLayout();
+        HorizontalLayout layout0=new HorizontalLayout();
+        ArrayList<VerticalLayout> chartList4=seisekiGraph.makeFourth();
+        ArrayList<VerticalLayout> chartList4B=seisekiGraph.makeBigFourth();
 
+        for(VerticalLayout chart: chartList4){
+            layout9.add(chart);
+        }
+        for(VerticalLayout chart: chartList4B){
+            layout0.add(chart);
+            chart.setVisible(false);
+        }
+
+
+        fourth.add(layout0);
+        fourth.add(layout9);
+
+
+        seiseki.getR6().addValueChangeListener(e -> {
+                    chartList4.get(0).setVisible(!(e.getValue().equals("学年全体")));
+                    chartList4B.get(0).setVisible(e.getValue().equals("学年全体"));
+                }
+        );
+        seiseki.getR6().addValueChangeListener(e -> {
+            chartList4.get(1).setVisible(!(e.getValue().equals("応用化学生物学科")));
+            chartList4B.get(1).setVisible(e.getValue().equals("応用化学生物学科"));
+        });
+        seiseki.getR6().addValueChangeListener(e -> {
+            chartList4.get(2).setVisible(!(e.getValue().equals("電子光工学科")));
+            chartList4B.get(2).setVisible(e.getValue().equals("電子光工学科"));
+
+        });
+        seiseki.getR6().addValueChangeListener(e -> {
+            chartList4.get(3).setVisible(!(e.getValue().equals("情報システム工学科")));
+            chartList4B.get(3).setVisible(e.getValue().equals("情報システム工学科"));
+        });
+
+        seiseki.getR6().addValueChangeListener(e -> {
+            if (e.getValue().equals("全て")) {
+                for (VerticalLayout chart : chartList4) {
+                    chart.setVisible(true);
+                }
+            }
+        });
         //ここまでfourth
 
         //ここから基本統計量
