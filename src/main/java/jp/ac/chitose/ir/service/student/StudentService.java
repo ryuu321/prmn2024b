6792e77ed7004520363c2084bf45bafa26e9d592
shun4193 <shun.h.1126.4193@rakuten.jp>
2024-05-17 16:38:19 +0900

    不要なクラスの削除

--- src/main/java/jp/ac/chitose/ir/service/student/StudentService.java
+++ src/main/java/jp/ac/chitose/ir/service/student/StudentService.java
@@ -16,14 +16,8 @@ public interface StudentService {
     @GetExchange("/grade/{studentNumber}/{subject}")
     TableData<StudentGrade> getStudentNumberGrade(@PathVariable String studentNumber, @PathVariable String subject);
 
-    @GetExchange("/subject_calc/{subject}")
-    TableData<StudentHist> getStudentHist(@PathVariable String subject);
-
     @GetExchange("/gpa_calc_par_year")
     TableData<StudentGPA> getStudentGPA();
-
-    @GetExchange("/table/{studentNumber}")
-    TableData<StudentTable> getStudentTable(@PathVariable String studentNumber);
 
     @GetExchange("/subject_calc_par_year/{subject}")
     TableData<StudentSubjectCalc> getStudentSubjectCalc(@PathVariable String subject);
