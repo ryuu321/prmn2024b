df1bcd2fa29ea57bde67df6c8410710300c070d7
shun4193 <shun.h.1126.4193@rakuten.jp>
2024-06-09 22:32:13 +0900

    アンケートのクラスを作成

--- src/main/java/jp/ac/chitose/ir/views/component/FatalErrorNotification.java
+++ src/main/java/jp/ac/chitose/ir/views/component/FatalErrorNotification.java
@@ -8,12 +8,16 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 
-public class ErrorNotification extends Notification {
-    public ErrorNotification(String text) {
+public class FatalErrorNotification extends Notification {
+    public FatalErrorNotification(String text) {
         addThemeVariants(NotificationVariant.LUMO_ERROR);
+        Button closeButton = new Button(new Icon("lumo", "cross"));
+        closeButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE);
+        closeButton.addClickListener(event -> {
+            close();
+        });
+        add(new HorizontalLayout(new Text(text), closeButton));
         setPosition(Position.TOP_CENTER);
-        setDuration(5000);
-        setText(text);
         open();
     }
 }
