914f77cb230f3172124453b85c56b1a34c84e605
katatsumuri <maimaidance0620@gmail.com>
2024-08-06 12:39:47 +0900

    ボタンの追加

--- src/main/java/jp/ac/chitose/ir/presentation/views/usermanagement/UserManagementButtons.java
+++ src/main/java/jp/ac/chitose/ir/presentation/views/usermanagement/UserManagementButtons.java
@@ -5,21 +5,32 @@ import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 public class UserManagementButtons extends VerticalLayout {
+    private Button userBulkAddButton;
+    private Button userAddButton;
+    private Button userDeleteButton;
+
     public UserManagementButtons() {
+        initializeButtons();
+        addComponentsToLayout();
     }
 
-    private void InitializeButtons() {
+    private void initializeButtons() {
         // ユーザーの一括追加ボタン
-        Button userBulkAddButton = new Button("一括追加");
+        this.userBulkAddButton = new Button("一括追加");
         userBulkAddButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         userBulkAddButton.setAutofocus(true);
 
         // ユーザーの追加ボタン
-        Button userAddButton = new Button("Secondary");
+        this.userAddButton = new Button("追加");
         userAddButton.setAutofocus(true);
 
         // ユーザーの削除ボタン
-        Button userDeleteButton = new Button("Secondary");
+        this.userDeleteButton = new Button("削除");
         userDeleteButton.addThemeVariants(ButtonVariant.LUMO_ERROR);
+    }
+
+    // 各種コンポーネントを画面に追加
+    private void addComponentsToLayout() {
+        add(userBulkAddButton, userAddButton, userDeleteButton);
     }
 }
--- src/main/java/jp/ac/chitose/ir/presentation/views/usermanagement/UserManagementTopView.java
+++ src/main/java/jp/ac/chitose/ir/presentation/views/usermanagement/UserManagementTopView.java
@@ -14,12 +14,15 @@ import jp.ac.chitose.ir.presentation.component.MainLayout;
 @RolesAllowed({"administrator"})
 public class UserManagementTopView extends VerticalLayout {
     private final UserManagementService userManagementService;
+    private final UserManagementButtons userManagementButtons;
     private final UsersDataGrid usersDataGrid;
+
 
     public UserManagementTopView(UserManagementService userManagementService) {
         add(new H1("ユーザー管理画面"));
         this.userManagementService = userManagementService;
+        userManagementButtons = new UserManagementButtons();
         usersDataGrid = new UsersDataGrid(userManagementService);
-        add(usersDataGrid);
+        add(userManagementButtons, usersDataGrid);
     }
 }
